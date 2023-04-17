1381fd8db85fe7e623cd228efae1c47f5ae2bd62
Gabriela Schmitt <86369677+GabrielaSchmitt@users.noreply.github.com>
2023-04-16 23:08:58 -0300

    Update _13 - Hash Passwords.py

--- _13 - Hash Passwords.py
+++ _13 - Hash Passwords.py
@@ -1,11 +1,11 @@
-# How to hash your passwords in python. Highly used in databases storage and validations. Here I`m using SHA256 encryptation. 
+# How to hash your passwords in python. Highly used in databases storage and validations. Here I`am using SHA256 encryption.
 from hashlib import sha256
 password = input("Password: ")
 
-# encoding for storaging
+# encoding for storing
 password = sha256(password.encode()).digest()
 
-# when trying to Login, the service must compare the hashed password with the hashed imput, as bellow:
+# when trying to Login, the service must compare the hashed password with the dashed input, as below:
 login_password = input("Password: ")
 hashed_login_password = sha256(login_password.encode()).digest()
 
