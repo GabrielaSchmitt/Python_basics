9dc8ffed00c65d180eb7342852f1ffd8ed47764d
Gabriela Schmitt <86369677+GabrielaSchmitt@users.noreply.github.com>
2023-06-01 21:46:43 -0300

    Update face_detection.py

--- face_detection.py
+++ face_detection.py
@@ -1,27 +1,34 @@
 import cv2
 import os
-from google.colab.patches import cv2_imshow
 
-cascade_path = '/content/haarcascade_frontalface_default.xml'
+# download the above doc by the link https://github.com/kipr/opencv/blob/master/data/haarcascades/haarcascade_frontalface_default.xml
+cascade_path = r'haarcascade_frontalface_default.xml'
 cascade_path = os.path.abspath(cascade_path)
 
 face_cascade = cv2.CascadeClassifier(cascade_path)
 
 try:
-    filename = take_photo()
-    filename = os.path.abspath(filename)
-    img = cv2.imread(filename)
+    cap = cv2.VideoCapture(0)
 
-    if img is None or img.shape[0] == 0 or img.shape[1] == 0:
-        print("Failed to load image")
-        exit()
+    while True:
+        ret, img = cap.read()
 
-    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    faces = face_cascade.detectMultiScale(gray, 1.1, 4)
-    for (x, y, w, h) in faces:
-        cv2.rectangle(img, (x, y), (x+w, y+h), (255, 0, 0), 2)
-    cv2_imshow(img)
-    cv2.waitKey(0)
+        if not ret:
+            print("Failed to capture image")
+            break
+
+        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        faces = face_cascade.detectMultiScale(gray, 1.1, 4)
+
+        for (x, y, w, h) in faces:
+            cv2.rectangle(img, (x, y), (x + w, y + h), (255, 0, 0), 2)
+
+        cv2.imshow("ASL Gesture Recognition", img)
+
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+
+    cap.release()
     cv2.destroyAllWindows()
 
 except Exception as err:
